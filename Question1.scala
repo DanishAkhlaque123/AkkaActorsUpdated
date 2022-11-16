d9641e3025e8290df6a9ca55581dafd1c80a203b
Danish Akhlaque <danishakhlaque@Danishs-MacBook-Pro.local>
2022-11-16 12:20:24 +0530

    16-11-2022

--- Question1.scala
+++ Question1.scala
@@ -1,10 +1,11 @@
-import Question1.writerInfo
+
 import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
 import akka.event.Logging
 
 import java.io.{BufferedWriter, File, FileWriter, PrintWriter}
+import java.time.LocalDateTime
 import scala.util.Try
-object Question1 extends App {
+object question1 extends App {
 
   object flush{
     val maxFlushCount = 2
@@ -12,6 +13,9 @@ object Question1 extends App {
     var countwarn = 0
   }
 
+  case class Info(message: String)
+  case class Warn(message: String)
+  case class Error(message: String)
   class actorWithInfo extends Actor with ActorLogging {
 
     import flush._
@@ -19,12 +23,15 @@ object Question1 extends App {
 
     override def receive: Receive = {
 
-      case message: String =>
+      case Info(message) =>
+        log.info(message)
         writeInfo(message)
 
 
-      case message: Int =>
+      case Warn(message) => log.warning(message)
         writeWarn(message)
+
+      case Error(message) => throw new RuntimeException(sender().toString + "error occured in this actor : message " + message)
     }
 
 
@@ -39,23 +46,26 @@ object Question1 extends App {
   }
 
 
-    val writerWarn: BufferedWriter = {
-      val f = new File("src/main/log/applicationWarnNew.log")
-      if (!f.exists()) {
-        f.createNewFile()
-      }
-
-      new BufferedWriter(new FileWriter(f, true))
+  val writerWarn: BufferedWriter = {
+    val f = new File("src/main/log/applicationWarnNew.log")
+    if (!f.exists()) {
+      f.createNewFile()
     }
+
+    new BufferedWriter(new FileWriter(f, true))
+  }
 
   val system = ActorSystem("LoggingQuestion1")
   val sampleActor = system.actorOf(Props(new actorWithInfo))
 
 
-  sampleActor ! "Hi my name is Danish"
-  //sampleActor ! "Hi my name is Tabish"
-  //sampleActor ! "Hi my name is Shahin"
-  sampleActor ! 42
+  sampleActor ! Info("Hi my name is Danish")
+
+  sampleActor ! Warn("Hi my name is Tabish")
+
+  sampleActor ! Warn("Hi my name is Tabish1")
+  sampleActor ! Warn("Hi my name is Tabish2")
+
 
 
 
@@ -74,14 +84,14 @@ object Question1 extends App {
 
   }
 
-  def writeWarn(str: Int) {
+  def writeWarn(str: String) {
 
     writerWarn.write(str)
     writerWarn.newLine()
     countwarn += 1
     if (countwarn > maxFlushCount) {
       writerWarn.flush()
-      Try(new File("src/main/log/applicationWarnNew.log").renameTo(new File("src/main/log/applicationWarnNew1.log"))).getOrElse(false)
+      Try(new File("src/main/log/applicationWarnNew.log").renameTo(new File(s"src/main/log/applicationWarnNew${ LocalDateTime.now()}.log"))).getOrElse(false)
       countwarn = 0
     }
 
@@ -91,5 +101,4 @@ object Question1 extends App {
   writerInfo.close
   writerWarn.close
 
-  }
-
+}
--- SupervisorSpec.scala
+++ SupervisorSpec.scala
@@ -1,61 +1,131 @@
-import akka.actor.SupervisorStrategy.Restart
-import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, OneForOneStrategy, Props}
+
+import akka.actor.SupervisorStrategy.Resume
+import akka.actor.{Actor, ActorLogging, ActorSystem, OneForOneStrategy, Props}
 import akka.event.Logging
 
-import java.io.{FileWriter, PrintWriter}
+import java.io.{BufferedWriter, File, FileWriter, PrintWriter}
+import scala.util.Try
+object SupervisorSpec extends App {
 
-object SupervisorSpec extends App{
+  case class CreateChild(value: String)
 
-  val system = ActorSystem("SampleActor")
-  object Parent{
-    case class startchild(value: String)
-    case class stopchild(message: String)
-    case object Stop
-  }
-
+  //case class tellchild(message: String)
 
   class Supervisor extends Actor with ActorLogging {
-    import Parent._
 
-    override def receive: Receive = withChildren(Map())
-    def withChildren(children: Map[String, ActorRef]): Receive={
-      case startchild(name)=>log.info(s"starting child ${name}")
-        context.become(withChildren(children+(name->context.actorOf(Props[child],name))))
+    override val supervisorStrategy =
+      OneForOneStrategy() {
+        case _: NullPointerException => Resume
+        case _: Exception => Resume
+      }
 
-      case stopchild(name) => log.info(s"stopping child ${name}")
-        val childOption = children.get(name)
-        childOption.foreach((childref=>context.stop(childref)))
-
-      case stop =>
-        log.info("stopping parent")
-        context.stop(self)
-
-        def supervisorStrategy = OneForOneStrategy() {
-          case _:RuntimeException=>Restart
-        }
-    }
-
-  }
-
-  class child extends Actor with ActorLogging{
-    val logger = Logging(context.system,this)
 
     override def receive: Receive = {
-      case  message: String => println("I am alive")
 
-      case message: Int => val fileWriter = new PrintWriter(new FileWriter("src/main/log/applicationWarn.log"))
-        fileWriter.write("hello warnning there" + message.toString)
-        fileWriter.close()
+      case CreateChild(name: String) => {
+
+        val ChildRef = context.actorOf(Props[actorWithInfo])
+
+        ChildRef ! Info("I am a warning log")
+      }
     }
 
   }
 
 
-  import Parent._
-  val parent = system.actorOf(Props[Supervisor],"parent")
-  parent ! startchild("child1")
-  val child = system.actorSelection("/user/parent/child1")
-  child ! 2
-  parent ! Stop
+  object flush{
+    val maxFlushCount = 2
+    var countInfo = 0
+    var countwarn = 0
+  }
+
+  case class Info(message: String)
+  case class Warn(message: String)
+  case class Error(message: String)
+  class actorWithInfo extends Actor with ActorLogging {
+
+    import flush._
+    val logger = Logging(context.system, this)
+
+    override def receive: Receive = {
+
+      case Info(message) =>
+        log.info(message)
+        writeInfo(message)
+        throw new NullPointerException("I am from Actor with warning")
+
+
+      case Warn(message) => log.warning(message)
+        writeWarn(message)
+
+      case Error(message) => throw new RuntimeException(sender().toString + "error occured in this actor : message " + message)
+    }
+
+
+  }
+
+  val writerInfo: BufferedWriter = {
+    val f = new File("src/main/log/applicationInfoNew.log")
+    if (!f.exists()) {
+      f.createNewFile()
+    }
+    new BufferedWriter(new FileWriter(f, true))
+  }
+
+
+  val writerWarn: BufferedWriter = {
+    val f = new File("src/main/log/applicationWarnNew.log")
+    if (!f.exists()) {
+      f.createNewFile()
+    }
+
+    new BufferedWriter(new FileWriter(f, true))
+  }
+
+  val system = ActorSystem("LoggingQuestion1")
+  val sampleActor = system.actorOf(Props(new actorWithInfo))
+
+
+ // sampleActor ! Info("Hi my name is Danish")
+
+ // sampleActor ! Warn("Hi my name is Tabish")
+
+  val systemNew = ActorSystem("Demo")
+  val parent = systemNew.actorOf(Props[Supervisor])
+  parent ! CreateChild("kid")
+
+
+
+  import flush._
+
+  def writeInfo(str: String) {
+
+    writerInfo.write(str)
+    writerInfo.newLine()
+    countInfo += 1
+    if (countInfo > maxFlushCount) {
+      writerInfo.flush()
+      Try(new File("src/main/log/applicationInfoNew.log").renameTo(new File("src/main/log/applicationInfoNew1.log"))).getOrElse(false)
+      countInfo = 0
+    }
+
+  }
+
+  def writeWarn(str: String) {
+
+    writerWarn.write(str)
+    writerWarn.newLine()
+    countwarn += 1
+    if (countwarn > maxFlushCount) {
+      writerWarn.flush()
+      Try(new File("src/main/log/applicationWarnNew.log").renameTo(new File("src/main/log/applicationWarnNew1.log"))).getOrElse(false)
+      countwarn = 0
+    }
+
+  }
+
+  Thread.sleep(5000)
+  writerInfo.close
+  writerWarn.close
 
 }
